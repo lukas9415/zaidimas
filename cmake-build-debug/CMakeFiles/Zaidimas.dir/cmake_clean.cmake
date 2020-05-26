0af97559a106b10112b6faed450a455b7cde8d29
lukas9415 <lukas.liutkevicius4@gmail.com>
2020-05-26 11:26:51 +0300

    Pirmasis git ikelimas

---
+++ cmake-build-debug/CMakeFiles/Zaidimas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Zaidimas.dir/main.cpp.obj"
+  "Zaidimas.exe"
+  "Zaidimas.exe.manifest"
+  "Zaidimas.pdb"
+  "libZaidimas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Zaidimas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Zaidimas.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Zaidimas.
+# This may be replaced when dependencies are built.
